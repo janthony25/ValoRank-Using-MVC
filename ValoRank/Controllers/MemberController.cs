57cac5dfceead6e6b397f205cec52b1a3ac788af
janthony25 <junanthony.business@gmail.com>
2024-02-20 20:52:28 +1300

    Finish Delete Member

--- ValoRank/Controllers/MemberController.cs
+++ ValoRank/Controllers/MemberController.cs
@@ -57,6 +57,33 @@ namespace ValoRank.Controllers
             return RedirectToAction("Index");
         }
 
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Members.Find(id);
+            return View(obj);
+        }
+
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Members.Find(id);
+            if(obj == null)
+            {
+                return NotFound(id);
+            }
+
+            _db.Members.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
