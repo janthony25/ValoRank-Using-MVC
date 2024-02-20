2d73220bfa70490b077717ce791a69dfa8529292
janthony25 <junanthony.business@gmail.com>
2024-02-20 20:07:01 +1300

    Create Member Controller

---
+++ ValoRank/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValoRank.Data;
+using ValoRank.Models;
+
+namespace ValoRank.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MemberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Member> members = _db.Members;
+
+            return View(members);
+        }
+    }
+}
