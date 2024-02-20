3c107cc2af4722eea48eb4ab4a23aa93c4ad7f51
janthony25 <junanthony.business@gmail.com>
2024-02-20 20:00:17 +1300

    Add DbContext

---
+++ ValoRank/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValoRank.Models;
+
+namespace ValoRank.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Member> Members { get; set; }
+    }
+}
