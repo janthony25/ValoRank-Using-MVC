5c909a378a86bf75d8861fdf119ca82767c088a1
janthony25 <junanthony.business@gmail.com>
2024-02-20 19:57:07 +1300

    Add Member Model

--- ValoRank/Models/Member.cs
+++ ValoRank/Models/Member.cs
@@ -4,6 +4,11 @@ namespace ValoRank.Models
 {
     public class Member
     {
-       
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string IGN { get; set; }
+        public string Rank { get; set; }
+        public int TagNo { get; set; }
     }
 }
